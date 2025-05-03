d768e4808fb57343491b60b293565cb80df1ed54
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-03 12:43:14 +0700

    Add markup for the url component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Url from "./components/url";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Url />
     </>
   );
 }
---
+++ src/components/url.jsx
@@ -0,0 +1,36 @@
+export default function Url() {
+  return (
+    <div>
+      <div className="urlContainer">
+        <div className="urlInputContainer">
+          <input type="text" placeholder="Shorten a link here" />
+          <span className="errorMessage"></span>
+        </div>
+        <div className="urlBtnContainer">
+          <button>Shorten It</button>
+        </div>
+      </div>
+      <div className="urlItemContainer">
+        <UrlItem
+          link={{ originalUrl: "originalUrl", shortenedUrl: "shortenedUrl" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function UrlItem({ link }) {
+  return (
+    <div>
+      <div>
+        <p>{link.originalUrl}</p>
+      </div>
+      <div>
+        <p>{link.shortenedUrl}</p>
+      </div>
+      <div>
+        <button>Copy</button>
+      </div>
+    </div>
+  );
+}
