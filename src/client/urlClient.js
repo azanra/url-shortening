70fca5c9219782e8aa227adce5bfe67436335ca7
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-15 12:13:49 +0700

    Add request to fetch inputted shortened url

---
+++ src/client/urlClient.js
@@ -0,0 +1,35 @@
+export default async function urlClient(originalUrl) {
+  const API_KEY =
+    "bdj4SGxmk5BMqSNxUF8Wxz8rhMB9iEIDKP304YVeTvMVomEixJSYjfNUGES1";
+  const API_URL = "https://api.tinyurl.com/create";
+  const data = {
+    url: originalUrl,
+  };
+  const requestOption = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  let shortenedUrl;
+
+  fetch(API_URL, requestOption)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed, response is not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      shortenedUrl = data.data.tiny_url;
+    })
+    .catch((error) => {
+      console.log("Error", error);
+    });
+  return shortenedUrl;
+}
