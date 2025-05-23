bbe09eca2b1c218cfc11b3cb6b03e6a150b9a576
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-23 11:37:04 +0700

    Style the nav text color and add button style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,18 +5,18 @@ export default function Header() {
     <div className="headerContainer flex justify-center p-8">
       <div className="header flex w-[80%] justify-between">
         <div className="nav flex">
-          <div className="headerlogo mr-8">
+          <div className="headerlogo mr-16 center">
             <img src={Logo} alt="Logo Image" />
           </div>
-          <div className="navLink center gap-4">
+          <div className="navLink center gap-8 text-(--Grayish-Violet)">
             <a href="">Features</a>
             <a href="">Pricing</a>
             <a href="">Resources</a>
           </div>
         </div>
-        <div className="action center gap-8">
-          <button>Login</button>
-          <button>Sign Up</button>
+        <div className="action center gap-16">
+          <button className="text-(--Grayish-Violet)">Login</button>
+          <button className="button">Sign Up</button>
         </div>
       </div>
     </div>
