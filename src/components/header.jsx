d940934acc7277c752ba7788d1d4a1a146d717d0
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-23 11:28:34 +0700

    Add gap and center it in x and y axis for header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,19 +2,19 @@ import Logo from "../assets/images/logo.svg";
 
 export default function Header() {
   return (
-    <div className="headerContainer flex justify-center">
+    <div className="headerContainer flex justify-center p-8">
       <div className="header flex w-[80%] justify-between">
         <div className="nav flex">
-          <div className="headerlogo">
+          <div className="headerlogo mr-8">
             <img src={Logo} alt="Logo Image" />
           </div>
-          <div className="navLink">
+          <div className="navLink center gap-4">
             <a href="">Features</a>
             <a href="">Pricing</a>
             <a href="">Resources</a>
           </div>
         </div>
-        <div className="action">
+        <div className="action center gap-8">
           <button>Login</button>
           <button>Sign Up</button>
         </div>
