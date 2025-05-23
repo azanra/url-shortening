a33406b0ec9d29f57841cbd84d76ac72f5e16f89
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-23 11:46:03 +0700

    Add hover state to the nav and button in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,18 +4,24 @@ export default function Header() {
   return (
     <div className="headerContainer flex justify-center p-8">
       <div className="header flex w-[80%] justify-between">
-        <div className="nav flex">
+        <div className="flex">
           <div className="headerlogo mr-16 center">
             <img src={Logo} alt="Logo Image" />
           </div>
           <div className="navLink center gap-8 text-(--Grayish-Violet)">
-            <a href="">Features</a>
-            <a href="">Pricing</a>
-            <a href="">Resources</a>
+            <a className="nav" href="">
+              Features
+            </a>
+            <a className="nav" href="">
+              Pricing
+            </a>
+            <a className="nav" href="">
+              Resources
+            </a>
           </div>
         </div>
         <div className="action center gap-16">
-          <button className="text-(--Grayish-Violet)">Login</button>
+          <button className="text-(--Grayish-Violet) nav">Login</button>
           <button className="button">Sign Up</button>
         </div>
       </div>
