659b57b338c5ba6f9a188229c68568ddfadf2602
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-24 12:32:12 +0700

    Add gap to the text in hero section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,11 +2,13 @@ import illustWorking from "../assets/images/illustration-working.svg";
 
 export default function Hero() {
   return (
-    <div className="flex center">
+    <div className="flex center py-24 px-8">
       <div className="flex w-[80%]">
         <div className="heroText">
           <div>
-            <h1 className="text-8xl font-bold">More than just shorter link</h1>
+            <h1 className="text-8xl font-bold mt-8 mb-4">
+              More than just shorter link
+            </h1>
           </div>
           <div className="w-[70%]">
             <p className="text-2xl text-(--Grayish-Violet)">
@@ -15,7 +17,7 @@ export default function Hero() {
             </p>
           </div>
           <div>
-            <button className="button">Get Started</button>
+            <button className="button mt-8">Get Started</button>
           </div>
         </div>
         <div className="heroImage">
