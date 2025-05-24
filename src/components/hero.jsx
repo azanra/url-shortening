7f0f3e5da4f0fcba4b47d417ed4d572d97fbb35d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-24 12:26:55 +0700

    Position and style the text and button in hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,20 +2,20 @@ import illustWorking from "../assets/images/illustration-working.svg";
 
 export default function Hero() {
   return (
-    <div>
-      <div>
+    <div className="flex center">
+      <div className="flex w-[80%]">
         <div className="heroText">
           <div>
-            <h1>More than just shorter link</h1>
+            <h1 className="text-8xl font-bold">More than just shorter link</h1>
           </div>
-          <div>
-            <p>
+          <div className="w-[70%]">
+            <p className="text-2xl text-(--Grayish-Violet)">
               Build your brand's recognition and get detailed insights on how
               your links are performing
             </p>
           </div>
           <div>
-            <button>Get Started</button>
+            <button className="button">Get Started</button>
           </div>
         </div>
         <div className="heroImage">
